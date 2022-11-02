8261eb448fc8e9c7276665b5fed449071981d5ca
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-11-03 06:48:21 +0900

    Update 3_👮🏻‍♂️팀 추노.py

--- pages/3_👮🏻‍♂️팀 추노.py
+++ pages/3_👮🏻‍♂️팀 추노.py
@@ -61,33 +61,32 @@ font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto,
 st.markdown(hide_menu, unsafe_allow_html=True)
 
 ################################# 팀 소개#############################################
-if select_event == '0️⃣ 팀 소개':
- 
-    st.markdown('<p align="left" style="font-family:나눔고딕 ExtraBold; color:black; font-size: 30px;">0️⃣ 팀 💪굳건히❓</p>', unsafe_allow_html=True)
-    st.markdown('<p align="left" style="font-family:나눔고딕; color:black; font-size: 15px;">장애인이 <b>굳건히</b> 자립적인 생활을 도모할 수 있도록 <b>국</b>민 <b>권</b>익 <b>위</b>원회에 지원한 팀 💪굳건히입니다.</p>', unsafe_allow_html=True)
-    st.markdown(' ')
-    st.markdown('---------------------------------------------------- ')
-    st.markdown('<p align="left" style="font-family:나눔고딕 ExtraBold; color:black; font-size: 30px;">팀원 소개</p>', unsafe_allow_html=True)
-    
-    # 페이지 레이아웃 3갈래
-    #팀원 소개
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.image("박지영.jpg", width=100)
-        st.markdown('<p align="left" style="font-family:나눔고딕; color:black; font-size: 15px;">팀장 : 이정민</p>', unsafe_allow_html=True)
-        st.markdown('<p align="left" style="font-family:나눔고딕; color:black; font-size: 15px;">경찰대학 치안대학원 범죄학과 석사과정</p>', unsafe_allow_html=True)
 
-    with col2:
-        st.image("증명사진_이정민.jpg", width=100)
-        st.markdown('<p align="left" style="font-family:나눔고딕; color:black; font-size: 15px;">팀원 : 이수연</p>', unsafe_allow_html=True)
-        st.markdown('<p align="left" style="font-family:나눔고딕; color:black; font-size: 15px;">경찰대학 법학과 3학년</p>', unsafe_allow_html=True)
+st.markdown('<p align="left" style="font-family:나눔고딕 ExtraBold; color:black; font-size: 30px;">0️⃣ 팀 💪굳건히❓</p>', unsafe_allow_html=True)
+st.markdown('<p align="left" style="font-family:나눔고딕; color:black; font-size: 15px;">장애인이 <b>굳건히</b> 자립적인 생활을 도모할 수 있도록 <b>국</b>민 <b>권</b>익 <b>위</b>원회에 지원한 팀 💪굳건히입니다.</p>', unsafe_allow_html=True)
+st.markdown(' ')
+st.markdown('---------------------------------------------------- ')
+st.markdown('<p align="left" style="font-family:나눔고딕 ExtraBold; color:black; font-size: 30px;">팀원 소개</p>', unsafe_allow_html=True)
 
-    with col3:
-        st.image("https://github.com/LAB-703/LAB-703/blob/main/%EB%B0%95%EC%98%81%EB%B9%88.jpg?raw=true", width=100)
-        st.markdown('<p align="left" style="font-family:나눔고딕; color:black; font-size: 15px;">팀원 : 김민정</p>', unsafe_allow_html=True)
-        st.markdown('<p align="left" style="font-family:나눔고딕; color:black; font-size: 15px;">경찰대학 법학과 4학년</p>', unsafe_allow_html=True)
-    st.markdown('---------------------------------------------------- ')   
-    st.markdown(' ')
-    st.markdown('<p align="left" style="font-family:나눔고딕; color:black; font-size: 15px;">※ 팀원 2명은 행정안전부와 한국지능정보사회진흥원에서 실시 중인 ‘공공빅데이터 분석 청년인재 양성사업’을 통해 국립재활원에 파견된 인턴연구원임</p>', unsafe_allow_html=True)
+# 페이지 레이아웃 3갈래
+#팀원 소개
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.image("박지영.jpg", width=100)
+    st.markdown('<p align="left" style="font-family:나눔고딕; color:black; font-size: 15px;">팀장 : 이정민</p>', unsafe_allow_html=True)
+    st.markdown('<p align="left" style="font-family:나눔고딕; color:black; font-size: 15px;">경찰대학 치안대학원 범죄학과 석사과정</p>', unsafe_allow_html=True)
+
+with col2:
+    st.image("증명사진_이정민.jpg", width=100)
+    st.markdown('<p align="left" style="font-family:나눔고딕; color:black; font-size: 15px;">팀원 : 이수연</p>', unsafe_allow_html=True)
+    st.markdown('<p align="left" style="font-family:나눔고딕; color:black; font-size: 15px;">경찰대학 법학과 3학년</p>', unsafe_allow_html=True)
+
+with col3:
+    st.image("https://github.com/LAB-703/LAB-703/blob/main/%EB%B0%95%EC%98%81%EB%B9%88.jpg?raw=true", width=100)
+    st.markdown('<p align="left" style="font-family:나눔고딕; color:black; font-size: 15px;">팀원 : 김민정</p>', unsafe_allow_html=True)
+    st.markdown('<p align="left" style="font-family:나눔고딕; color:black; font-size: 15px;">경찰대학 법학과 4학년</p>', unsafe_allow_html=True)
+st.markdown('---------------------------------------------------- ')   
+st.markdown(' ')
+st.markdown('<p align="left" style="font-family:나눔고딕; color:black; font-size: 15px;">※ 팀원 2명은 행정안전부와 한국지능정보사회진흥원에서 실시 중인 ‘공공빅데이터 분석 청년인재 양성사업’을 통해 국립재활원에 파견된 인턴연구원임</p>', unsafe_allow_html=True)
     
 
