c954ae1b8c618972fc798e4bb6a0a174a362ce13
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-11-05 01:46:16 +0900

    Update 3_🧭공간 EDA.py

--- pages/3_🧭공간 EDA.py
+++ pages/3_🧭공간 EDA.py
@@ -107,7 +107,6 @@ st.markdown(hide_menu, unsafe_allow_html=True)
 st.markdown('<p align="center" style=" font-size: 140%;"><b>🧭공간 EDA</b></p>', unsafe_allow_html=True)
 
 
-expander
 expander1 = st.expander("시군구 신고량 상위 10개")
 expander1.table(pd.read_excel("시군구_신고량, 면적당 신고량 파일.xlsx", sheet_name=0))
 expander2 = st.expander("시군구 면적당 신고량 상위 10개")
