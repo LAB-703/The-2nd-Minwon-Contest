3c277bc299179129b8ac274a93c15769625df556
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-11-06 19:07:58 +0900

    Update 3_🧭공간 EDA.py

--- pages/3_🧭공간 EDA.py
+++ pages/3_🧭공간 EDA.py
@@ -155,12 +155,13 @@ m
 # print(source_code)
 # components.html(source_code, height=450,  scrolling=False)
 
-
+st.image("시도별 신고량 순(최종).jpg")
 expander1 = st.expander("시도 신고량(신고량 순)")
 expander1.dataframe(pd.read_csv("시도_신고량순.csv",encoding='cp949',index_col=0), use_container_width=True)
 expander1.write("*면적당 신고량=신고량/면적")
 
-expander2 = st.expander("시군구 신고량(신고량 순)")
+st.image("시도별 면적당 신고량 순(최종).jpg")
+expander2 = st.expander("시군구 면적당 신고량(면적당 신고량 순)")
 expander2.dataframe(pd.read_csv("시군구_면적별신고량순(최종).csv",encoding='cp949'), use_container_width=True)
 expander2.write("*면적당 신고량=신고량/면적")
 
